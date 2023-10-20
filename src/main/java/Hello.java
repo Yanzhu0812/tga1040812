b2cf980b38056d5d3bf7ef11264ed66fe6d30b19
YanZhu <yanzhu355@gmail.com>
2023-10-20 11:49:17 +0800

    h

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -14,6 +14,7 @@ public class Hello extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Hello from Servlet");
+		out.println("Hello from Servlet");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
